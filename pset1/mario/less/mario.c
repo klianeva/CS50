a0b27354567c7f8d22f82a3e811cef9bdda904f2
Viktoria Klianeva <viktoria.klianeva@code.berlin>
2018-10-03 16:24:49 +0000

    commit test file

---
+++ pset1/mario/less/mario.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+# include <stdio.h>
+
+int main (void)
+{
+  int n;
+    do
+    {
+        n = get_int("Positive number less than 23: ");
+    }
+    while (n <= 0 || n > 23);
+
+  for (int i = 4; i <= 4; i++)
+
+   printf(" \n",i);
+}
