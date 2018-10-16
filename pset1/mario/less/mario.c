a2b2cf188d9638c1ba1328554815deb5f5bccf13
Viktoria Klianeva <viktoria.klianeva@code.berlin>
2018-10-16 17:09:17 +0000

    Initial commit

--- pset1/mario/less/mario.c
+++ pset1/mario/less/mario.c
@@ -3,14 +3,24 @@
 
 int main (void)
 {
-  int n;
+  int n; //height
+  int j;
     do
     {
         n = get_int("Positive number less than 23: ");
     }
-    while (n <= 0 || n > 23);
+  while (n <= 0 || n >= 23);
 
-  for (int i = 4; i <= 4; i++)
-
-   printf(" \n",i);
+    //For loop for the rows
+    for (int i = 0; i < n; i++){
+        // For loop for the spaces and hashtags
+        for(j = 0; j < n-1-i; j++){
+            printf(" ");
+        }
+        for(j = 0; j < i + 1; j++){
+            printf("#");
+        }
+    printf("\n");
+    }
+    return 0;
 }
